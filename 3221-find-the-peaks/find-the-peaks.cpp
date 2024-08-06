519762243300c2387b40fc000961b345235b0b4c
meet0503 <125305515+meet0503@users.noreply.github.com>
2024-08-07 00:03:40 +0530

    Time: 13 ms (7.14%) | Memory: 37.2 MB (12.45%) - LeetSync

--- 3221-find-the-peaks/find-the-peaks.cpp
+++ 3221-find-the-peaks/find-the-peaks.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
-    vector<int> findPeaks(vector<int>& mountain) {
-        vector<int> ans;
-        for(int i = 1 ; i<mountain.size()-1; i++){
-            if(mountain[i]>mountain[i+1] && mountain[i]>mountain[i-1]){
-                ans.push_back(i);
+    
+    void binary_search(vector<int> &answer, vector<int> mountain, int left, int right){
+        
+        if(left <= right){
+            int mid = (left + right) / 2;
+            
+           if(mountain[mid] > mountain[mid + 1] && mountain[mid] > mountain[mid - 1]){
+                answer.push_back(mid);
             }
+            
+            binary_search(answer, mountain, left , mid - 1);
+            binary_search(answer, mountain, mid + 1, right);
         }
-        return ans;
+    }
+    
+    vector<int> findPeaks(vector<int>& mountain) {
+        
+        vector<int> answer;
+        binary_search(answer, mountain, 1, mountain.size() - 2);
+        
+        return answer;
     }
 };
