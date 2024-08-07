42db9134e9a4986def5fe13a9f1c3ccbaffdcec0
meet0503 <125305515+meet0503@users.noreply.github.com>
2024-08-08 02:29:00 +0530

    Time: 17 ms (67.53%) | Memory: 12.6 MB (51.97%) - LeetSync

---
+++ String/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int i=0;
+        string ans="";
+        while(i<s.size()){
+            if(ans.length() >0 && ans[ans.length()-1]==s[i]){
+                ans.pop_back();
+            }
+            else{
+                ans.push_back(s[i]);
+            }
+            i++;
+        }
+        return ans;
+    }
+};
