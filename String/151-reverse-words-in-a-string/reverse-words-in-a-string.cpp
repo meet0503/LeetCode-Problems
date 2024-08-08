766852e9069469f4238d3d5aa117bc52b3700af0
meet0503 <125305515+meet0503@users.noreply.github.com>
2024-08-08 11:37:49 +0530

    Time: 7 ms (35.70%) | Memory: 10.2 MB (43.73%) - LeetSync

--- String/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ String/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,38 +2,32 @@ class Solution {
 public:
     string reverseWords(string s) {
         vector<string> ans;
-        int n = s.size();
-        int pos = 0;
-
-        // Skip leading spaces
-        while (pos < n && s[pos] == ' ') {
+        int pos=0;
+        while(s[pos] == ' '){
             pos++;
         }
-
-        int nexPos = s.find(' ', pos);
-        while (nexPos != string::npos) {
-           
-                ans.push_back(s.substr(pos, nexPos - pos));
-        
-            pos = nexPos + 1;
-            while (pos < n && s[pos] == ' ') {
+        int nexPos = s.find(' ',pos);
+        while(nexPos != string::npos){
+            ans.push_back(s.substr(pos, nexPos-pos));
+            
+            pos = nexPos+1;
+            while(s[pos] == ' '){
                 pos++;
-            }
+            }  
             nexPos = s.find(' ', pos);
-        }
-
-        if (pos < n) {
+            
+        } 
+        if (pos < s.size()) {
             ans.push_back(s.substr(pos));
         }
-
-        string result = "";
-        for (int i = ans.size() - 1; i >= 0; i--) {
-            result += ans[i];
-            if (i > 0) {
+        string result="";
+        for(int i=ans.size()-1; i>=0; i--){
+            result+= ans[i];
+            if(i>0){
                 result += " ";
             }
         }
-
+        
         return result;
     }
 };
