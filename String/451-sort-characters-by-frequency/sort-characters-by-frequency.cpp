6f7b2ae48231368a496f22090181f1bbcb837b01
meet0503 <125305515+meet0503@users.noreply.github.com>
2024-08-10 01:20:37 +0530

    Time: 5 ms (94.94%) | Memory: 9.9 MB (93.87%) - LeetSync

---
+++ String/451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findmaxIndex(int arr[]){
+        int  max =0;
+        int index = 0;
+        for(int i=0; i<256; i++){
+            if(arr[i]>max){
+                max = arr[i];
+                index = i; 
+            }  
+        }
+        return index;
+    }
+
+    string frequencySort(string s) {
+        int s_hash[256]={0};
+        int elem = 0;
+        for(int i=0; i<s.size(); i++){
+            s_hash[s[i]]++;
+            elem++;
+        }
+        string ans="";
+        int j=0;
+        while(j<elem){
+            int I_max = findmaxIndex(s_hash);
+            char ch = I_max;
+            for(int i=s_hash[I_max]; i>0; i--){
+                ans += ch;
+                j++;
+            }
+            s_hash[I_max]=0;
+        }
+        return ans;
+    }
+};
