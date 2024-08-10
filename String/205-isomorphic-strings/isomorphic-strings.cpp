7f7a03c7c0798db1349146176d3a11454d77041e
meet0503 <125305515+meet0503@users.noreply.github.com>
2024-08-10 23:38:12 +0530

    Time: 3 ms (90.67%) | Memory: 8.6 MB (68.91%) - LeetSync

--- String/205-isomorphic-strings/isomorphic-strings.cpp
+++ String/205-isomorphic-strings/isomorphic-strings.cpp
@@ -6,8 +6,8 @@ public:
 
         for (int i = 0; i < s.size(); i++) {
             if(s_hash[s[i]] != t_hash[t[i]]) return false;
-            s_hash[s[i]]=i+1;
-            t_hash[t[i]]=i+1;
+            s_hash[s[i]]= i+1;
+            t_hash[t[i]] = i+1;
         }
 
         return true;
